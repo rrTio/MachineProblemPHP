7016382b6ccb5dbeddb0c1100b2546d2980d09fd
rrTio <tio.rozsraven@ue.edu.ph>
2021-10-22 01:16:12 +0800

    Update

--- configurations/checklogin.php
+++ configurations/checklogin.php
@@ -1,40 +1,17 @@
 <?php
-    require_once '../configurations/config.php';
-            
+    include_once 'configurations/config.php';
+
     $studentNumber = $_POST['sNumber'];
     $password = $_POST['password'];
 
-    if(empty($studentNumber) && empty($password)){
-        echo '<script type="text/javascript">',
-            'alert("ERROR. INVALID USERNAME OR PASSWORD");',
-            'window.location.href="../index.php?error=no%20inputs"',
-            '</script>';
+    if(empty($studentNumber) || empty($password))
+    {
+        header("Location: ../landing.php?login=failed");
     }
 
-    if(!empty($studentNumber) && !empty($password)){
-        
-        if($sql = "SELECT * FROM student_info WHERE (user_firstName) = '$studentNumber' AND (user_password) = '$password';")
-        {
-            mysqli_query($conn, $sql);
-            header("Location: ../landing.php?login=success");
-        }
-
-        if($sql = "SELECT * FROM student_info WHERE (user_firstName) = '$studentNumber' AND (user_password) != '$password';")
-        {
-            mysqli_query($conn, $sql);
-            echo '<script type="text/javascript">',
-            'alert("ERROR. INVALID USERNAME OR PASSWORD");',
-            'window.location.href="../index.php?error=inputs"',
-            '</script>';
-        }
-
-        if($sql = "SELECT * FROM student_info WHERE (user_firstName) != '$studentNumber' AND (user_password) = '$password';")
-        {
-            mysqli_query($conn, $sql);
-            echo '<script type="text/javascript">',
-            'alert("ERROR. INVALID USERNAME OR PASSWORD");',
-            'window.location.href="../index.php?error=inputs"',
-            '</script>';
-        }
+    else{
+        $sql = "SELECT * FROM student_info WHERE (user_firstName) = '$studentNumber' AND (user_password) = '$password';";
+        mysqli_query($conn, $sql);
+        header("Location: ../landing.php?login=success");
     }
 ?>
--- forgotpassword.php
+++ forgotpassword.php
@@ -1,51 +1,47 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>Login</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="assets/css/forgotPassword.css" />
-    <script src='assets/js/newlink.js'>
-    </script>
-</head>
-
-<body>
-    <center>
-        <div class="container">
-            <h4>Forgot your password?</h4>
-            <p>You can change it here</p>
-            <form action='configurations/checklogin.php' method="POST">
-                <div class="row">
-                    <div class="col-md-12 col-12-mobile">
-                        <input pattern="\d*" maxlength="11" id="sNumber" name="sNumber" placeholder="Student Number">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 col-12-mobile">
-                        <input type="password" id="password" name="password" placeholder="Password">
-                    </div>
-                    <div class="col-md-12 col-12-mobile">
-                        <input type="password" id="confirmPassword" name="confirmPassword"
-                            placeholder="Confirm Password">
-                    </div>
-                </div>
-
-                <center>
-                    <button type="submit" class="custom-btn btn-10 ">Change Password</button>
-                </center>
-            </form>
-        </div>
+    <head>
+        <title>Forgot Password</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="assets/css/forgotPassword.css" />
+        <script src='assets/js/newlink.js'>
+        </script>
+    </head>
+    <body>
         <center>
-            <button type="submit" onClick='openlink()' class="custom-btn btn-10 btnCreate" id="newAccount">Create New
-                Account
-            </button>
+            <div class="container">
+                <h4>Forgot your password?</h4>
+                <p>You can change it here</p>
+                <form action='configurations/checklogin.php' method="POST">
+                    <div class="row">
+                        <div class="col-md-12 col-12-mobile">
+                            <input pattern="\d*" maxlength="11" id="sNumber" name="sNumber" placeholder="Student Number" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-12-mobile">
+                            <input type="password" id="password" name="password" placeholder="Password" required>
+                        </div>
+                        <div class="col-md-12 col-12-mobile">
+                            <input type="password" id="confirmPassword" name="confirmPassword"
+                                placeholder="Confirm Password" required>
+                        </div>
+                    </div>
+                    <center>
+                        <button type="submit" class="custom-btn btnChange ">Change Password</button>
+                    </center>
+                </form>
+            </div>
+            <center>
+                <button type="submit" onClick='openlink()' class="custom-btn btn-10 btnCreate" id="newAccount">Create New
+                    Account
+                </button>
+            </center>
         </center>
-    </center>
-</body>
-
+    </body>
 </html>
